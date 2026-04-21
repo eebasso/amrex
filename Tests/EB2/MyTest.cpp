7031db872c9da4f7504b0ad0864ca4c6957990bc
Edward Basso <edwardebasso@gmail.com>
2026-04-21 09:40:14 -0700

    Updates to MyTest

--- Tests/EB2/MyTest.cpp
+++ Tests/EB2/MyTest.cpp
@@ -7,10 +7,12 @@
 #include <AMReX_PlotFileUtil.H>
 #include <AMReX_EB2.H>
 
+using namespace amrex;
+
 MyTest::MyTest ()
 {
     init_arrays();
-    test_set_eb_data();
+    test_eb2();
 }
 
 void
@@ -18,83 +20,124 @@ MyTest::init_arrays ()
 {
     Box domain(IntVect(AMREX_D_DECL(0,0,0)), IntVect(AMREX_D_DECL(0,0,0)));
 
-    geom.define(domain);
+    m_geom.define(domain);
     dx_iso = GpuArray<Real,AMREX_SPACEDIM>{AMREX_D_DECL(0.2, 0.2, 0.2)};
     dx_aniso = GpuArray<Real,AMREX_SPACEDIM>{AMREX_D_DECL(0.3, 0.5, 0.7)};
 
-    box_arr.define(domain);
-    dmap.define(box_arr);
+    m_box_arr.define(domain);
+    m_dmap.define(m_box_arr);
 
     MFInfo mfinfo{};
     IntVect ng{0};
     // mfinfo.SetTag("Tests::EB2::MyTest");
 
-    m_cellflag.define(box_arr, dmap, 1, ng, mfinfo);
-    m_volfrac.define(box_arr, dmap, 1, ng, mfinfo);
-    m_volcent.define(box_arr, dmap, AMREX_SPACEDIM, ng, mfinfo);
+    m_cellflag.define(m_box_arr, m_dmap, 1, ng, mfinfo);
+    m_volfrac.define(m_box_arr, m_dmap, 1, ng, mfinfo);
+    m_volcent.define(m_box_arr, m_dmap, AMREX_SPACEDIM, ng, mfinfo);
 
-    m_bndryarea.define(box_arr, dmap, 1, ng, mfinfo);
-    m_bndrycent.define(box_arr, dmap, AMREX_SPACEDIM, ng, mfinfo);
-    m_bndrynorm.define(box_arr, dmap, AMREX_SPACEDIM, ng, mfinfo);
+    m_bndryarea.define(m_box_arr, m_dmap, 1, ng, mfinfo);
+    m_bndrycent.define(m_box_arr, m_dmap, AMREX_SPACEDIM, ng, mfinfo);
+    m_bndrynorm.define(m_box_arr, m_dmap, AMREX_SPACEDIM, ng, mfinfo);
 
     for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
 
-        BoxArray box_arr_fcent = amrex::convert(box_arr, IntVect::TheDimensionVector(idim));
+        BoxArray box_arr_fcent = amrex::convert(m_box_arr, IntVect::TheDimensionVector(idim));
 
-        m_areafrac[idim].define(box_arr_fcent, dmap, 1, ng, mfinfo);
-        m_facecent[idim].define(box_arr_fcent, dmap, AMREX_SPACEDIM-1, ng, mfinfo);
+        m_areafrac[idim].define(box_arr_fcent, m_dmap, 1, ng, mfinfo);
+        m_facecent[idim].define(box_arr_fcent, m_dmap, AMREX_SPACEDIM-1, ng, mfinfo);
 
         IntVect edge_type{1};
         edge_type[idim] = 0;
-        BoxArray box_arr_ecent = amrex::convert(box_arr, edge_type);
+        BoxArray box_arr_ecent = amrex::convert(m_box_arr, edge_type);
 
-        m_edgecent[idim].define(box_arr_ecent, dmap, 1, ng, mfinfo);
+        m_edgecent[idim].define(box_arr_ecent, m_dmap, 1, ng, mfinfo);
     }
-
-    // const EB2::IndexSpace& eb_is = EB2::IndexSpace::top();
-    // const EB2::Level& eb_level = eb_is.getLevel(geom);
-    // factory = std::make_unique<EBFArrayBoxFactory>
-    //     (eb_level, geom, box_arr, dmap, Vector<int>{2,2,2}, EBSupport::full);
-    // factory = std::make_unique<EBFArrayBoxFactory>();
-
-    // m_levelset.define(box_arr, dmap, 1, 0, MFInfo());
-    // m_cellflag.define(box_arr, dmap, 1, 0, MFInfo());
-
-    // m_volfrac.define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-    // m_volcent.define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-
-    // m_bndryarea.define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-    // m_bndrycent.define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-    // m_bndrynorm.define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-
-    // for (int d = 0; d < AMREX_SPACEDIM; ++d) {
-    //     m_areafrac[d].define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-    //     m_facecent[d].define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-    //     m_edgecent[d].define(box_arr, dmap, 1, 0, MFInfo(), *factory);
-    // }
 }
 
 void
-MyTest::test_set_eb_data ()
+MyTest::test_eb2 ()
 {
     Real apxm = Real(0.3);
     Real apxp = Real(1.0);
     Real apym = Real(0.4);
     Real apyp = Real(1.0);
 
-    for (MFIter mfi(box_arr, dmap); mfi.isValid(); ++mfi)
+    for (MFIter mfi(m_box_arr, m_dmap); mfi.isValid(); ++mfi)
     {
         const Box& bx = mfi.validbox();
         const Box& nbx = amrex::surroundingNodes(bx);
         Array4<Real> const& apx_arr = m_apx.array(mfi);
         Array4<Real> const& apy_arr = m_apy.array(mfi);
 
+        auto& gfab = m_mgf[mfi];
+        const Box& vbx = gfab.validbox();
 
-        amrex::EB2::set_eb_data(
+        auto& levelset = gfab.getLevelSet();
+        if (iter == 0) {
+            gshop.fillFab(levelset, m_geom, gshop_run_on, bounding_box);
+#ifdef AMREX_USE_GPU
+            if (hybrid) {
+                levelset.prefetchToDevice();
+            }
+#endif
+        }
+
+        auto& cellflag = m_cellflag[mfi];
+
+        gfab.buildTypes(cellflag);
+
+        const Array4<const Real> & clst = levelset.const_array();
+        const Array4<Real> &  lst = levelset.array();
+        const Array4<EBCellFlag> &cfg = m_cellflag.array(mfi);
+        const Array4<Real> &vfr = m_volfrac.array(mfi);
+        const Array4<Real> &ctr = m_volcent.array(mfi);
+        const Array4<Real> &bar = m_bndryarea.array(mfi);
+        const Array4<Real> &bct = m_bndrycent.array(mfi);
+        const Array4<Real> &bnm = m_bndrynorm.array(mfi);
+
+        auto& facetype = gfab.getFaceType();
+        AMREX_D_TERM(
+            Array4<Real> const& apx = m_areafrac[0].array(mfi);
+            Array4<Real> const& fcx = m_facecent[0].array(mfi);
+            Array4<EB2::Type_t> const& ftx = facetype[0].array();
+            ,
+            Array4<Real> const& apy = m_areafrac[1].array(mfi);
+            Array4<Real> const& fcy = m_facecent[1].array(mfi);
+
+            ,
+            Array4<Real> const& apz = m_areafrac[2].array(mfi);
+            Array4<EB2::Type_t> const& ftz = facetype[2].array();
+        );
+        AMREX_D_TERM(
+            ,
+            Array4<Real> const& fcz = m_facecent[2].array(mfi);
+
+        );
+
+        AMREX_D_TERM(
+            ,
+            Array4<EB2::Type_t> const& fty = facetype[1].array();,
+
+        );
+
+#if AMREX_SPACEDIM == 2
+        EB2::build_faces
+
+
+        // EB2::set_eb_data(
+        //     0, 0, apx_arr, apy_arr, dx_aniso,
+        //     vcent, bcent, levset
+        // )
+#else
+        EB2::set_eb_data(
             0, 0, apx_arr, apy_arr, dx_aniso,
-            vfrac,
+            vcent, bcent, levset
         )
+#endif
+
     }
+
+
 }
 
 void
